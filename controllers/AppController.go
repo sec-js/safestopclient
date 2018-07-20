1f1d909be86711526b3d808694eb73008773837b
Jeremie Weldin <jweldin@jweldin.com>
2018-07-20 16:50:30 -0400

    addAction magic.

--- controllers/AppController.go
+++ controllers/AppController.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 	"github.com/schoolwheels/safestopclient/models"
+	"strings"
+	"reflect"
 )
 
 type AppController struct {
@@ -13,20 +15,22 @@ func (c *AppController) Register() {
 
 	//templates
 	c.addTemplateApp("index")
-	c.addTemplateApp("account")
-	c.addTemplateApp("map")
 
 	//actions
-	c.addRouteWithPrefix("/", c.indexAction)
-	c.addRouteWithPrefix("/account", c.accountAction)
-	c.addRouteWithPrefix("/map", c.mapAction)
+	c.addRouteWithPrefix("/", c.IndexAction)
+	c.addAction("activate" )
+	c.addAction("account" )
+	c.addAction("faq" )
+	c.addAction("language" )
+	c.addAction("map" )
+
 }
 
 type dashData struct {
 	CurrentUserEmail string
 }
 
-func (c *AppController) indexAction(w http.ResponseWriter, r *http.Request) {
+func (c *AppController) IndexAction(w http.ResponseWriter, r *http.Request) {
 	session, _ :=  c.SessionStore.Get(r, "auth")
 	email := session.Values["current_user_email"]
 	if email != nil {
@@ -36,7 +40,7 @@ func (c *AppController) indexAction(w http.ResponseWriter, r *http.Request) {
 	c.render(w, r, "index", nil)
 }
 
-func (c *AppController) accountAction(w http.ResponseWriter, r *http.Request) {
+func (c *AppController) AccountAction(w http.ResponseWriter, r *http.Request) {
 	session, _ :=  c.SessionStore.Get(r, "auth")
 	email := session.Values["current_user_email"]
 	if email != nil {
@@ -46,7 +50,22 @@ func (c *AppController) accountAction(w http.ResponseWriter, r *http.Request) {
 	c.render(w, r, "account", nil)
 }
 
-func (c *AppController) mapAction(w http.ResponseWriter, r *http.Request) {
+func (c *AppController) ActivateAction(w http.ResponseWriter, r *http.Request) {
+	session, _ :=  c.SessionStore.Get(r, "auth")
+	email := session.Values["current_user_email"]
+	if email != nil {
+		http.Redirect(w, r, r.URL.Host+"/dashboard", http.StatusFound)
+	}
+
+	c.render(w, r, "activate", nil)
+}
+
+func (c *AppController) LanguageAction(w http.ResponseWriter, r *http.Request) {
+	c.render(w, r, "language", nil)
+}
+
+
+func (c *AppController) MapAction(w http.ResponseWriter, r *http.Request) {
 	session, _ :=  c.SessionStore.Get(r, "auth")
 	email := session.Values["current_user_email"]
 	if email != nil {
@@ -54,6 +73,16 @@ func (c *AppController) mapAction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.render(w, r, "map", nil)
+}
+
+func (c *AppController) FaqAction(w http.ResponseWriter, r *http.Request) {
+	session, _ :=  c.SessionStore.Get(r, "auth")
+	email := session.Values["current_user_email"]
+	if email != nil {
+		http.Redirect(w, r, r.URL.Host+"/dashboard", http.StatusFound)
+	}
+
+	c.render(w, r, "faq", nil)
 }
 
 
@@ -88,4 +117,12 @@ func (c *AppController) getCurrentUser(r *http.Request) *models.User {
 	email := c.getCurrentUserEmail(r)
 	user := models.FindUserByEmail(email)
 	return user
+}
+
+// addAction requires you to have a view named <action>.html and a method func (c *AppController) <Action>Action(http.ResponseWriter, *http.Request)
+func (c *AppController) addAction(action string){
+	//TODO: determine if this can be moved to ControllerBase and if c can just be cast to the correct type.
+	//fmt.Println(strings.Title(action)+"Action")
+	c.addTemplateApp(action)
+	c.Router.HandleFunc(c.RoutePrefix+"/"+action, reflect.ValueOf(c).MethodByName(strings.Title(action)+"Action").Interface().(func(http.ResponseWriter, *http.Request)))
 }
